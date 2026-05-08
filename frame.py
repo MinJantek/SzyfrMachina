8707a96f4d9ad797ff08cc6597f61ee7d8cf465e
MinJantek <antek.sowy@gmail.com>
2026-05-08 18:40:33 +0200

    kończeke głuwnych mechanik ramek

--- frame.py
+++ frame.py
@@ -1,14 +1,27 @@
 import pygame
 from setup import *
-
+import copy
 class Frame(pygame.sprite.Sprite):
     def __init__(self,coordinates,file):
         super().__init__()
+        self.file = file
         self.image = pygame.image.load(f"{IMAGE_PATH}/{file}")
-        self.rect = self.image.get_rect(center = (0,0))
-        self.rect = pygame.Rect(0,0,50,50)
+        self.rect = self.image.get_rect(topleft = coordinates)
+    def render(self,picked,picked2,name):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            self.image = pygame.image.load(f"{IMAGE_PATH}/{self.file}")
+            self.mask = True
+            if name == picked or name == picked2: self.mask = False
+            if self.mask: self.image.fill((130,130,130),special_flags=pygame.BLEND_ADD)
+            a = f"screen.blit({name}.image,{name}.rect)"
+            
+            return a
+        if name == picked or name == picked2: return f"screen.blit({name}.image,{name}.rect)"
+        
+        
+         
+        
     
-    def check_collisions(self):
-        return self.rect.collidepoint(pygame.mouse.get_pos())
+    
             
 
--- project.py
+++ project.py
@@ -4,16 +4,41 @@ from setup import *
 from frame import Frame
 screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 background = pygame.image.load(f"{IMAGE_PATH}/SzyfrMachina_background.png")
+picked = "frame2"
+picked2 = "frame7"
 active = True
-frame1 = Frame((0,0),"SzyfrMachina_50.50.png")
+frame1 = Frame((20,20),"SzyfrMachina_200.60.png")
+frame2 = Frame((220,20),"SzyfrMachina_200.60.png")
+frame3 = Frame((420,20),"SzyfrMachina_200.60.png")
+frame4 = Frame((620,20),"SzyfrMachina_200.60.png")
+frame5 = Frame((820,20),"SzyfrMachina_200.60.png")
+frame6 = Frame((1020,20),"SzyfrMachina_200.60.png")
+
+frame7 = Frame((230,106),"SzyfrMachina_1000.300.png")
+frame8 = Frame((230,406),"SzyfrMachina_1000.300.png")
 while active:
     screen.blit(background,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            active = False
-    if frame1.rect.collidepoint(pygame.mouse.get_pos()):
-        print("Antek")
-        screen.blit(frame1.image,(20,20))
+            if event.key == pygame.K_ESCAPE:
+                active = False
+                print("________tekst szyfru_______")
+                print("________tekst szyfru_______")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(1,7):
+                if eval(f"frame{i}.rect.collidepoint(pygame.mouse.get_pos())"):
+                    picked = f"frame{i}"
+                    print("ANTEk")
+                    break
+            for i in range(7,9):
+                if eval(f"frame{i}.rect.collidepoint(pygame.mouse.get_pos())"):
+                    picked2 = f"frame{i}"
+                    print("ANTEkkkkkk")
+                    break
+    for i in range(1,9):
+        a = eval(f"frame{i}.render('{picked}','{picked2}','frame{i}')")
+        eval(f"{a}")
+
 
     
     pygame.display.flip()
